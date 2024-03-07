4028bddf92ddc13fa8c8b10894889f7c0199fe8e
sonia <https://github.com/Soniabensaad>
2024-03-07 21:52:30 +0100

    cinema-guru

---
+++ holbertonschool-cinema-guru/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Your content goes here */}
+    </div>
+  );
+}
+
+export default App;
