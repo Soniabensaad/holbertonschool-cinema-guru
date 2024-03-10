7aa5e4e5c453c54cc3e455425fd9e8a96da52876
sonia <https://github.com/Soniabensaad>
2024-03-10 22:07:51 +0100

    cinema

--- holbertonschool-cinema-guru/src/App.js
+++ holbertonschool-cinema-guru/src/App.js
@@ -1,11 +1,35 @@
-// src/App.js
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import App from './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const postData = async () => {
+      const response = await fetch('/api/auth/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + accessToken
+        },
+        body: JSON.stringify({ key: 'value' })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setIsLoggedIn(true);
+        setUserUsername(data.username);
+      }
+    };
+
+    postData();
+  }, []);
+
   return (
     <div className="App">
-      {/* Your content goes here */}
+      {isLoggedIn ? <Dashboard /> : <Authentication />}
     </div>
   );
 }
