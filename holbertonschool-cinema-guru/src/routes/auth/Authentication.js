6c72eedfb555eeb028fd99b676c5bb016c5201af
sonia <https://github.com/Soniabensaad>
2024-03-12 22:46:53 +0100

    cinema

--- holbertonschool-cinema-guru/src/routes/auth/Authentication.js
+++ holbertonschool-cinema-guru/src/routes/auth/Authentication.js
@@ -1,16 +1,41 @@
 import React, { useState } from 'react';
-import './auth.css';
-import Login from './Login';
-import Register from './Register';
+import axios from 'axios';
 
 const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
   const [_switchBoolean, set_switchBoolean] = useState(true);
   const [usernamestring, setUsernamestring] = useState("");
   const [passwordstring, setPasswordstring] = useState("");
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      let response;
+      if (_switchBoolean) {
+        response = await axios.post('/api/auth/login', { username: usernamestring, password: passwordstring });
+      } else {
+        response = await axios.post('/api/auth/register', { username: usernamestring, password: passwordstring });
+      }
+
+      const { token } = response.data;
+
+      
+      localStorage.setItem('token', token);
+
+     
+      setUserUsername(usernamestring);
+
+    
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.error('Error:', error);
+      
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <button onClick={() => set_switchBoolean(true)}>
           Sign In
         </button>
